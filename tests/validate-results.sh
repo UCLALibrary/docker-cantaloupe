d6388ed6ec98cb31839d56d233618ca4d807b81e
Hardy Pottinger <hardy.pottinger@gmail.com>
2019-02-04 14:35:56 -0600

    WIP, add test for whether port 8182 is exposed

--- tests/validate-results.sh
+++ tests/validate-results.sh
@@ -11,11 +11,11 @@ die () {
   echo ""
   printf "${RED}Tests failed!${NC}\n"
   echo ""
-  echo "$message at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]}." >&2
+  echo "$message" >&2
 
   if [ "$TRAVIS" != 'true' ]; then
     printf "  ${GREEN}To clean up the testing container, run:${NC}\n"
-    echo "    docker rm -f \"$(cat $1)\""
+    echo "    docker rm -f \"$container_id\""
     echo ""
     printf "  ${GREEN}To clean up all system containers, run:${NC}\n"
     echo "    docker rm -f \$(docker ps -a -q)"
@@ -27,15 +27,15 @@ die () {
 
 # These are the actual tests we run
 
-$container_id = $(/bin/cat "$1")
+container_id="$(cat $1)"
 
 #smoke test, this will die if the container doesn't start up or if /etc/cantaloupe.properties doesn't exist in the container
 docker exec --tty "${container_id}" env TERM=xterm ls /etc/cantaloupe.properties >> /dev/null 2>&1 || die "$1" "$?"
 
 # verify that port 8182 is exposed in the container, NOTE this doesn't guarantee anything is listening
 exposed_ports="$(docker inspect --format='{{range $p, $conf := .Config.ExposedPorts}} {{$p}} {{end}}' $container_id)"
-if ! [[ "${exposed_ports}" =~ '.*8182.*' ]] ; then
-  die "$1" "$?"
+if ! [[ "${exposed_ports}" =~ '8182' ]] ; then
+  die "port 8182 is not exposed in container_id $container_id" "$?"
 fi
 
 printf "${GREEN}Tests ran successfully!${NC}\n"
