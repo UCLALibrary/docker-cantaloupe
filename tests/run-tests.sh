fab7b9b7da44b2b2cb3002762c9e58a1802c5526
Hardy Pottinger <hardy.pottinger@gmail.com>
2019-02-04 14:14:41 -0600

    WIP, and failing at it

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,21 +5,6 @@ GREEN='\033[1;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
-# An informational message in case a test fails
-die () {
-  echo ""
-  printf "${RED}Tests failed!${NC}\n"
-  echo ""
-  printf "  ${GREEN}To clean up the testing container, run:${NC}\n"
-  echo "    docker rm -f \"$(cat $1)\""
-  echo ""
-  printf "  ${GREEN}To clean up all system containers, run:${NC}\n"
-  echo "    docker rm -f \$(docker ps -a -q)"
-  echo ""
-
-  exit 1
-}
-
 # Our tests need Docker installed
 hash docker 2>/dev/null || { echo >&2 "I require Docker to be installed to run tests. Aborting."; exit 1; }
 
--- tests/validate-results.sh
+++ tests/validate-results.sh
@@ -7,9 +7,11 @@ NC='\033[0m'
 
 # An informational message in case a test fails
 die () {
+  local message=$1
   echo ""
   printf "${RED}Tests failed!${NC}\n"
   echo ""
+  echo "$message at ${BASH_SOURCE[1]}:${FUNCNAME[1]} line ${BASH_LINENO[0]}." >&2
 
   if [ "$TRAVIS" != 'true' ]; then
     printf "  ${GREEN}To clean up the testing container, run:${NC}\n"
@@ -24,6 +26,16 @@ die () {
 }
 
 # These are the actual tests we run
-docker exec --tty "$(cat ${1})" env TERM=xterm ls /etc/cantaloupe.properties >> /dev/null 2>&1 || die "$1" "$?"
+
+$container_id = $(/bin/cat "$1")
+
+#smoke test, this will die if the container doesn't start up or if /etc/cantaloupe.properties doesn't exist in the container
+docker exec --tty "${container_id}" env TERM=xterm ls /etc/cantaloupe.properties >> /dev/null 2>&1 || die "$1" "$?"
+
+# verify that port 8182 is exposed in the container, NOTE this doesn't guarantee anything is listening
+exposed_ports="$(docker inspect --format='{{range $p, $conf := .Config.ExposedPorts}} {{$p}} {{end}}' $container_id)"
+if ! [[ "${exposed_ports}" =~ '.*8182.*' ]] ; then
+  die "$1" "$?"
+fi
 
 printf "${GREEN}Tests ran successfully!${NC}\n"
