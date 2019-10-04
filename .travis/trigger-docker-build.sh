acccdd35ebda8940d5d81298c0d802c37da3d432
Anthony Vuong <vuonga91@gmail.com>
2019-10-04 13:25:25 -0700

    Add docker trigger

---
+++ .travis/trigger-docker-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# The following vars are stored encrypted in TravisCI
+# JENKINS_USER
+# JENKINS_USER_PASS
+# JENKINS_URL: Contains Token
+
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+  curl -I -u "${JENKINS_USER}:${JENKINS_USER_PASS}" "${JENKINS_URL}&GIT_COMMIT_HASH=${TRAVIS_COMMIT}"
+fi
+
