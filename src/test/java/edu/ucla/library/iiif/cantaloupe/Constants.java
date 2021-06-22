07a981c88dc9adf4143587f10bfe481f20f7a201
Kevin S. Clarke <ksclarke@ksclarke.io>
2021-06-22 18:21:40 -0400

    Fix commit hash builds + bump Cantaloupe version (#114)

--- src/test/java/edu/ucla/library/iiif/cantaloupe/Constants.java
+++ src/test/java/edu/ucla/library/iiif/cantaloupe/Constants.java
@@ -12,6 +12,9 @@ public final class Constants {
     /* The version of Cantaloupe that's being tested. */
     public static final String CANTALOUPE_VERSION = "cantaloupe.version";
 
+    /* The Cantaloupe commit hash that's being tested. */
+    public static final String CANTALOUPE_COMMIT_REF = "cantaloupe.commit.ref";
+
     /* The version of Kakadu that's being tested. */
     public static final String KAKADU_VERSION = "kakadu.version";
 
